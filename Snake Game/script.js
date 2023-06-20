98c16daefd73641ef07723e5f47265a5d824e43b
sekar N <sekarnagarajan1234@gmail.com>
2023-06-20 22:25:08 +0530

    javascript

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,7 +1,4 @@
 const playBoard = document.querySelector(".play-board");
-const scoreElement = document.querySelector(".score");
-const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelector(".controls i");
 
 let foodX = 13,
   foodY = 10;
