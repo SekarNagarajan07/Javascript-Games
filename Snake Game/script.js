7c553515b4845a0ef7ba377dd4b48af01c421f05
sekar N <sekarnagarajan1234@gmail.com>
2023-06-20 22:24:11 +0530

    #

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -6,9 +6,19 @@ const controls = document.querySelector(".controls i");
 let foodX = 13,
   foodY = 10;
 
+let snakeX = 5,
+  snakeY = 10;
+
+const changeFoodPosition = () => {
+  //passing a random 0 - 30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
 const initGame = () => {
-  let htmlMarkup = `<div class = "food" style = "grid-area: ${foodX} / ${foodY}"> </div>`;
+  let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"> </div>`;
+
+  htmlMarkup += `<div class = "head" style = "grid-area: ${snakeY} / ${snakeX}"> </div>`;
   playBoard.innerHTML = htmlMarkup;
 };
-
+changeFoodPosition();
 initGame();
