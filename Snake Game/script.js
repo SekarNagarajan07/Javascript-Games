87fa73cce556c974214f96df8c76165f7a58c0f7
sekar N <sekarnagarajan1234@gmail.com>
2023-06-21 14:26:09 +0530

    js

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,5 +1,6 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver = false;
 let foodX = 13,
   foodY = 10;
 
@@ -34,13 +35,20 @@ const changeDirection = (e) => {
   }
 };
 const initGame = () => {
+  if (gameOver) return handleGameOver();
   let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"> </div>`;
 
   //Checking if the snake hit the food
   if (snakeX === foodX && snakeY === foodY) {
     changeFoodPosition();
     snakeBody.push([foodX, foodY]); //pushing food position to snake body array.
-    console.log(snakeBody);
+
+    // console.log(snakeBody);
+  }
+
+  //Shifting forward the values of the elements in the snake body by one.
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
   }
 
   snakeBody[0] = [snakeX, snakeY]; //setting first element of snake body to current snake position.
@@ -49,6 +57,10 @@ const initGame = () => {
   snakeX += velocityX;
   snakeY += velocityY;
 
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    // console.log("Game Over!!");
+    gameOver = true;
+  }
   for (let i = 0; i < snakeBody.length; i++) {
     //Adding a div for each part of the snake's body
     htmlMarkup += `<div class = "head" style = "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"> </div>`;
